c5ddcabda069c3dcd0bd61cb52a4dc738fbfec27
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2025-01-10 12:29:38 +0800

    initial next

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center">SoundQueue</h1>
+      <p className="text-lg text-center">Please select your link here</p>
+      <input />
+    </>
+  );
+}
